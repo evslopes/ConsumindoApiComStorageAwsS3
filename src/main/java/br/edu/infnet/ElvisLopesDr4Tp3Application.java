cb59ba8603eba1aa2802a8d1ed3519f1b1fdee07
Elvis Lopes <evs.lopes@gmail.com>
2022-03-23 21:22:55 -0300

    Initial commit

---
+++ src/main/java/br/edu/infnet/ElvisLopesDr4Tp3Application.java
@@ -0,0 +1,13 @@
+package br.edu.infnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElvisLopesDr4Tp3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElvisLopesDr4Tp3Application.class, args);
+    }
+
+}
